c6143025669ab5f9f7146935ea7b25908af98c8d
Jeffrey Chen <Jeffreychen14898@gmail.com>
2021-05-12 13:24:17 -0500

    wrote the pseudocode for the timer

---
+++ javascript/timer.js
@@ -0,0 +1,36 @@
+class Timer {
+	constructor() {
+		//
+	}
+}
+
+/*
+Timer wait
+Timer time
+bool started
+
+void startFunc() {
+	started = true;
+	green text
+	animationframe
+}
+
+void animationframe() {
+}
+
+void keydown
+	if(started)
+		time stopped
+	else
+		start time
+
+void keyup
+	if(!started)
+		check wait time
+			start time
+			reset wait time
+			startFunc();
+		else
+			reset wait time
+			black text
+*/
