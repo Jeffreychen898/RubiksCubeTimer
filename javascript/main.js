a426976a19a57ab03ae3105c2f8e96c88983e91c
Jeffrey Chen <Jeffreychen14898@gmail.com>
2021-05-16 19:15:06 -0500

    created a CountUp class to store anything related to the timer

--- javascript/main.js
+++ javascript/main.js
@@ -13,84 +13,25 @@ const CONSTANTS = {
 			}
 		},
 		SCRAMBLE: {
-			MOVES: 20
+			MOVES: 15
 		}
 	}
 }
 
-const scramble_element = $("scramble");
-const timer_element = $("timer");
-
-let wait = new Timer();
-let timer_millis = 0;
-let timer = new Timer();
-
-let solve_record = [];
+const countup = new CountUp();
 
 function setup() {
 	CONSTANTS.ELEMENTS.SCRAMBLE.innerHTML = generateScramble(CONSTANTS.PROPERTIES.SCRAMBLE.MOVES).join(" ");
 }
 
-function keyPressed() {
-	if(timer.started)
-		stopTimer();
-	else if(!wait.started) {
-		wait.start();
-		CONSTANTS.ELEMENTS.TIMER.style.color = CONSTANTS.PROPERTIES.TIMER.COLORS.WAIT;
-		runAnimation();
-	}
+function keyPressed(event) {
+	if(event.keyCode == 32)
+		countup.keyPressed();
 }
 
 function keyReleased() {
-	if(!timer.started) {
-		if(wait.getTime() > CONSTANTS.PROPERTIES.TIMER.WAIT_TIME) {
-			timer.start();
-			wait.reset();
-			startTimer();
-		} else {
-			wait.reset();
-			CONSTANTS.ELEMENTS.TIMER.style.color = CONSTANTS.PROPERTIES.TIMER.COLORS.STOP;
-		}
-	}
-}
-
-function runAnimation() {
-	if(wait.getTime() > CONSTANTS.PROPERTIES.TIMER.WAIT_TIME)
-		CONSTANTS.ELEMENTS.TIMER.style.color = CONSTANTS.PROPERTIES.TIMER.COLORS.RUN;
-
-	if(timer.started) {
-		CONSTANTS.ELEMENTS.TIMER.innerHTML = readableTime(timer.getTime());
-		timer_millis = timer.getTime();
-	}
-
-	if(wait.started || timer.started)
-		requestAnimationFrame(runAnimation);
-}
-
-function startTimer() {
-	CONSTANTS.ELEMENTS.TIMER.style.color = CONSTANTS.PROPERTIES.TIMER.COLORS.RUN;
-}
-
-function stopTimer() {
-	recordSolve();
-	timer.reset();
-	CONSTANTS.ELEMENTS.SCRAMBLE.innerHTML = generateScramble(CONSTANTS.PROPERTIES.SCRAMBLE.MOVES).join(" ");
-}
-
-function recordSolve() {
-	const parent_element = $("solve-container");
-	parent_element.innerHTML += `<li class="each-solve">${readableTime(timer_millis)}</li>`;
-	solve_record.push({ time: timer_millis, scramble: CONSTANTS.ELEMENTS.SCRAMBLE.innerHTML });
-}
-
-function readableTime(millis) {
-	let centisec = Math.floor(millis / 10) % 100;
-	let seconds = Math.floor(millis / 1000) % 60;
-	let minutes = Math.floor(millis / 60000);
-
-	centisec = (centisec > 9)?centisec:"0" + centisec;
-	const minutes_result = (minutes > 0)?minutes + ":":"";
-	return minutes_result + seconds + "." + centisec;
+	if(event.keyCode == 32)
+		countup.keyReleased();
 }
 
 /* element events */
--- javascript/timer.js
+++ javascript/timer.js
@@ -1,3 +1,82 @@
+class CountUp {
+	constructor() {
+		this.countup_timer = 0;
+		this.wait = new Timer();
+		this.timer = new Timer();
+
+		this.solves_record = [];
+	}
+
+	keyPressed() {
+		if(this.timer.started)
+			this.timerStop();
+		else if(!this.wait.started) {
+			this.wait.start();
+			CONSTANTS.ELEMENTS.TIMER.style.color = CONSTANTS.PROPERTIES.TIMER.COLORS.WAIT;
+			this.runAnimationFrame();
+		}
+	}
+
+	keyReleased() {
+		if(!this.timer.started) {
+			if(this.wait.getTime() > CONSTANTS.PROPERTIES.TIMER.WAIT_TIME) {
+				this.timer.start();
+				this.wait.reset();
+				this.timerStart();
+			} else {
+				this.wait.reset();
+				CONSTANTS.ELEMENTS.TIMER.style.color = CONSTANTS.PROPERTIES.TIMER.COLORS.STOP;
+			}
+		}
+	}
+
+	readableTime(millis) {
+		let centisec = Math.floor(millis / 10) % 100;
+		let seconds = Math.floor(millis / 1000) % 60;
+		let minutes = Math.floor(millis / 60000);
+
+		centisec = (centisec > 9)?centisec:"0" + centisec;
+		const minutes_result = (minutes > 0)?minutes + ":":"";
+		return minutes_result + seconds + "." + centisec;
+	}
+
+	/* @private */
+	timerStart() {
+		CONSTANTS.ELEMENTS.TIMER.style.color = CONSTANTS.PROPERTIES.TIMER.COLORS.RUN;
+	}
+
+	timerStop() {
+		this.recordSolve();
+		this.timer.reset();
+		CONSTANTS.ELEMENTS.SCRAMBLE.innerHTML = generateScramble(CONSTANTS.PROPERTIES.SCRAMBLE.MOVES).join(" ");
+	}
+
+	runAnimationFrame() {
+		if(this.wait.getTime() > CONSTANTS.PROPERTIES.TIMER.WAIT_TIME)
+			CONSTANTS.ELEMENTS.TIMER.style.color = CONSTANTS.PROPERTIES.TIMER.COLORS.RUN;
+
+		if(this.timer.started) {
+			CONSTANTS.ELEMENTS.TIMER.innerHTML = this.readableTime(this.timer.getTime());
+			this.countup_timer = this.timer.getTime();
+		}
+
+		if(this.wait.started || this.timer.started) {
+			requestAnimationFrame(() => {
+				this.runAnimationFrame();
+			});
+		}
+	}
+
+	recordSolve() {
+		const parent_element = $("solve-container");
+		parent_element.innerHTML += `<li class="each-solve">${this.readableTime(this.countup_timer)}</li>`;
+		this.solves_record.push({
+			time: this.countup_timer,
+			scramble: CONSTANTS.ELEMENTS.SCRAMBLE.innerHTML
+		});
+	}
+}
+
 class Timer {
 	constructor() {
 		this.started = false;
